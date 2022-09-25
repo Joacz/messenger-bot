1971fec412ad3f2aea84e6dae92a8e62e99345f8
Joaquin Gomez <joagomez2007@gmail.com>
2022-09-25 09:46:07 -0300

    Agregado estilo de la página principal

--- bot-de-mensajeria-front/src/App.js
+++ bot-de-mensajeria-front/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Nav from './components/Nav';
 import NavItem from './components/NavItem';
+import User from './components/User';
 
 function App() {
   return (
@@ -16,6 +17,41 @@ function App() {
           CATEGORIAS
         </NavItem>
       </Nav>
+      <div className='contenedor-principal'>
+        <div className='contenedor-categoria'></div>
+        <div className='contenedor-titulo'>
+          <div className='titulo'>
+            <h1>Bienvenido/a</h1>
+            <p>
+              Puedes configurar los mensajes por categoría y usuario, aquí
+              tienes una lista de mensajes predeterminados
+            </p>
+          </div>
+          <div className='contenedor-usuarios'>
+            <h2>PERFIL</h2>
+            <User name='Joaquín' email='joaquin@example.com' isAdmin={true} />
+            <h2>USUARIOS</h2>
+            <User
+              name='Gustavo Fring'
+              email='gusfring@example.com'
+              isAdmin={false}
+            />
+            <User
+              name='Philip J. Fry'
+              email='philipfry@example.com'
+              isAdmin={false}
+            />
+            <User
+              name='Walter White'
+              email='walt@example.com'
+              isAdmin={false}
+            />
+            <User name='Mom' email='mommy1@example.com' isAdmin={false} />
+            <User name='Boss' email='boss@example.com' isAdmin={false} />
+            <User name='Lawyer' email='mylawyer@example.com' isAdmin={false} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ bot-de-mensajeria-front/src/components/User.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../styles/User.css';
+import UserImage from '../svg/user.svg';
+import AdminImage from '../svg/admin.svg';
+
+function User({ name, email, isAdmin }) {
+  if (!isAdmin) {
+    return (
+      <div className='user'>
+        <div className='user-public-data'>
+          <img id='user-img' src={UserImage} alt='user' />
+          <div className='user-data'>
+            <h3>{name}</h3>
+            <span>{email}</span>
+          </div>
+        </div>
+        <div className='separator'></div>
+        <div className='user-actions'>
+          <button className='button'>Borrar</button>
+          <button className='button'>Editar</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className='user'>
+        <div className='user-public-data'>
+          <img id='admin-img' src={AdminImage} alt='user' />
+          <div className='user-data'>
+            <h3>{name}</h3>
+            <span>{email}</span>
+          </div>
+        </div>
+        <div className='separator'></div>
+        <div className='user-actions'>
+          <button className='button'>Perfil</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
